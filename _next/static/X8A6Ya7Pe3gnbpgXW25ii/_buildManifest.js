872733fb98ebc506653e8fd61ae3c45616ab651c
davidcaptart-glitch <da.vid.captart@gmail.com>
2026-05-04 16:11:23 +0200

    Updates

--- _next/static/X8A6Ya7Pe3gnbpgXW25ii/_buildManifest.js
+++ _next/static/X8A6Ya7Pe3gnbpgXW25ii/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/yoteempujo//_next/:path+",
-        "destination": "/yoteempujo/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
